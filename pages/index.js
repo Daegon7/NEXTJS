79ee77eada0f18f193ee0939d15356f0c5348a64
daegon7 <kdg8302@gmail.com>
2025-06-23 14:47:16 +0900

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import styles from '../css/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.main}>
+      <h1>Welcome to Next.js!</h1>
+    </div>
+  );
+}
