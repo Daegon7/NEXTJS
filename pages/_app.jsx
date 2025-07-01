9310a9ac56ad2851213e6675b91b4493c321fe58
daegon7 <kdg8302@gmail.com>
2025-07-01 14:10:30 +0900

    update css

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,8 @@
 import { ApolloProvider } from '@apollo/client';
 import client from '../lib/apollo-client';
 
+import '@/css/globals.css'
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
